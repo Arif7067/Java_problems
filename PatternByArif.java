48447a3d649a0a7849836c94bba1698d18b69c33
ARIF  HASSAN <75191282+Arif7067@users.noreply.github.com>
2024-03-14 21:36:16 +0530

    12 updated patterns

--- PatternByArif.java
+++ PatternByArif.java
@@ -6,7 +6,15 @@ public class PatternByArif {
 //        pattern2(5);
 //        pattern3(5);
 //        pattern4(5);
-        pattern5(9);
+//        pattern5(9);
+//        pattern6(5);
+//        pattern7(5);
+//        pattern8(5);
+//        pattern9(5);
+//        pattern10(5);
+//        pattern11(5);
+        pattern12(5);
+
 
     }
     static void pattern1(int n){
@@ -73,6 +81,111 @@ public class PatternByArif {
 
                 System.out.println();
             }
+        }
+
+    }
+    static void pattern6(int n){
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                if(j<=n-i) {
+                    System.out.print(" ");
+                } else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern7(int n){
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                if(j<i) {
+                    System.out.print(" ");
+                } else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern8(int n){
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=2*n;j++){
+                if(j<=n-i) {
+                    System.out.print(" ");
+                } else if (n-i<j && j<n+i) {
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern9(int n){
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<2*n;j++){
+                if(j<i) {
+                    System.out.print(" ");
+                } else if (j<=2*n-i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern10(int n){
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                if(j<=n-i) {
+                    System.out.print(" ");
+                } else if (n-i<j && j<=n) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern11(int n){
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                if(j<i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern12(int n){
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                if(j<i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                if(j<=n-i) {
+                    System.out.print(" ");
+                } else if (n-i<j && j<=n) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
         }
 
     }
