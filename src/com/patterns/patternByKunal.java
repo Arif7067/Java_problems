fd91ccdc8cea3e9537170df09577b3b245e8526f
ARIF  HASSAN <75191282+Arif7067@users.noreply.github.com>
2024-03-14 19:48:54 +0530

    pattern2

--- src/com/patterns/patternByKunal.java
+++ src/com/patterns/patternByKunal.java
@@ -1,4 +1,17 @@
 package com.patterns;
 
 public class patternByKunal {
+    public static void main(String[] args){
+        pattern2(5);
+
+    }
+    static void pattern2(int n){
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
 }
